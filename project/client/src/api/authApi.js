55663bd3e0c223bbb2567be9ef94daced46705dd
BMO2004 <basmala.ysalem@gmail.com>
2025-10-12 17:04:15 +0300

    Login works but need jsonwebtoken

--- project/client/src/api/authApi.js
+++ project/client/src/api/authApi.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+// Make sure the URL matches your backend port
+const API_URL = "http://localhost:3000/api/auth"; 
 
 export const loginUser = async (email, password) => {
   const res = await axios.post(`${API_URL}/login`, { email, password });
+  return res.data;
+};
+
+export const signupUser = async (userData) => {
+  const res = await axios.post(`${API_URL}/signup`, userData);
   return res.data;
 };
