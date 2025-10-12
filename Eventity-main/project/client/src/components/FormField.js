751f61170c6a85f6db41612126381ae61e240c9a
bedoemad <bedoemad155@gmail.com>
2025-10-12 15:45:13 +0300

    Fixed merge error

--- Eventity-main/project/client/src/components/FormField.js
+++
@@ -1,9 +0,0 @@
-export default function FormField({ label, children, error }) {
-  return (
-    <div className="field">
-      <label>{label}</label>
-      {children}
-      {error ? <div className="error">{error}</div> : null}
-    </div>
-  );
-}
